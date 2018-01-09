248244a69353b9e3716df77823d3cb4c7f1fc7e9
raymond-yuan <ray.yuan0@gmail.com>
2018-01-09 10:11:09 -0600

    rnn embed baseline

---
+++ src/config.py
@@ -0,0 +1,5 @@
+max_features = 20000
+maxlen = 100
+
+batch_size = 32
+epochs = 2
---
+++ src/models/rnn_embed.py
@@ -0,0 +1,25 @@
+
+
+# Any results you write to the current directory are saved as output.
+
+from keras.models import Model
+from keras.layers import Dense, Embedding, Input
+from keras.layers import LSTM, Bidirectional, GlobalMaxPool1D, Dropout
+from config import *
+
+def get_model():
+    embed_size = 128
+    inp = Input(shape=(maxlen, ))
+    x = Embedding(max_features, embed_size)(inp)
+    x = LSTM(50, return_sequences=True)(x)
+    x = GlobalMaxPool1D()(x)
+    x = Dropout(0.1)(x)
+    x = Dense(50, activation="relu")(x)
+    x = Dropout(0.1)(x)
+    x = Dense(6, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
