d3b0f06fd749d8534443d3b8170125fc543c0be6
Raymond Yuan <ray.yuan0@gmail.com>
2018-02-08 22:36:13 -0600

    some garbage

--- src/models/rnn_embed.py
+++ src/models/rnn_embed.py
@@ -40,3 +40,23 @@ def get_GRU_model(embedding_matrix):
                   metrics=['accuracy'])
 
     return model
+
+def get_GRU_Max_model(embedding_matrix):
+    # embed_size = 128
+    inp = Input(shape=(maxlen, ))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = Bidirectional(GRU(50, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
+    x = GlobalMaxPool1D()(x)
+    max_in = Dropout(0.1)(x)
+    max_1 = Dense(256)(max_in)
+    max_1 = Dense(256)(max_in)
+
+    x = Dense(256, activation="relu")(x)
+    x = Dropout(0.1)(x)
+    x = Dense(6, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
