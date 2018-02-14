2fcbc0a53c2fc93601faa605158331776354a074
Ubuntu <ubuntu@ip-172-31-24-181.ec2.internal>
2018-02-14 06:49:09 +0000

    config

--- src/config.py
+++ src/config.py
@@ -4,22 +4,23 @@ TRAIN_DATA_FILE = "../data/train.csv"
 TEST_DATA_FILE = "../data/test.csv"
 # EMBEDDING_FILE = "../data/glove.6B.50d.txt"
 # EMBEDDING_FILE = "../data/wiki.simple.vec"
-EMBEDDING_FILE = "../data/wiki.en.vec"
+EMBEDDING_FILE = "../data/wiki.en.bin"
 
 embed_type = EMBEDDING_FILE.split('/')[-1]
 
 model_name = 'GRU_Ensemble'
-embedding_type = 'fasttextLim'
-MODEL_DIR = "./submissions/{}-{}-{}-{}/".format(embedding_type, embed_type, model_name, str(datetime.now()).replace(' ', '-'))
+embedding_type = 'fasttext'
+MODEL_DIR = "./submissions/{}-{}-{}-{}/".format(embedding_type, embed_type, model_name,
+                                                str(datetime.now()).replace(' ', '-'))
 
 data_augmentors = ["train_de.csv", "train_fr.csv", "train_es.csv"]
 # data_augmentors = []
 # MODEL_DIR = '/home/raymond/Documents/projects/toxic-comments/src/submissions/fasttextLim-wiki.en.vec-GRU_Ensemble-2018-02-09 22:19:07.545168'
 # max_features = 100000  # Number of unique words
-maxlen = 200  # max number of words in a comment to use
+maxlen = 500  # max number of words in a comment to use
 embed_size = 300  # Size of each word vector (default value)
 
-batch_size = 128
+batch_size = 256
 epochs = 6
 
 ensemble_num = 1
